c025d9468937e7fc6f15f0664733d32702e7c8fb
comarniceanugeorge <62394043+georgecomarniceanu@users.noreply.github.com>
2024-10-02 13:50:34 +0300

    Update hubspot-form.js

--- hubspot-form.js
+++ hubspot-form.js
@@ -1,2 +1,7 @@
 const firstName = document.getElementById('firstname-f777a803-462f-448e-bc85-2012617daebf');
-firstName.style.backgroundColor = '#F3F2F4';
+
+function updatePeopleSliderValue() {
+  firstName.style.backgroundColor = '#F3F2F4';
+}
+
+updatePeopleSliderValue();
