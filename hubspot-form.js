d8d563d8dea0b4b52505fb9ae69efd42317cb3c0
comarniceanugeorge <62394043+georgecomarniceanu@users.noreply.github.com>
2024-10-02 14:30:37 +0300

    Update hubspot-form.js

--- hubspot-form.js
+++ hubspot-form.js
@@ -1,7 +1,1 @@
-const firstName = document.getElementsByClassName('input')
 
-function updatePeopleSliderValue() {
-  firstName[0].style.backgroundColor = '#F3F2F4';
-}
-
-updatePeopleSliderValue();
