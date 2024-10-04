4d25b645aa5e1b480fef4aeed783cdfff61e3119
comarniceanugeorge <62394043+georgecomarniceanu@users.noreply.github.com>
2024-10-04 16:17:26 +0300

    Update hubspot-form.js

--- hubspot-form.js
+++ hubspot-form.js
@@ -1,1 +1,3 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#label-number_of_employees-f777a803-462f-448e-bc85-2012617daebf span").textContent = "Number of employees in org";
+});
